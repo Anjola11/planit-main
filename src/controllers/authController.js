8aea02d854860b54f829d3609e04bac5d800ecb2
Aanu <aladeniyiaanu@gmail.com>
2025-11-07 17:35:45 +0100

    fix 2 bcrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import TokenManager from '../utils/tokenManager.js';
 import { User } from '../models/User.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 export const authenticate = async (req, res, next) => {
   try {
